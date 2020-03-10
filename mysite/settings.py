5d74748e9d22343571059c8f4763ff232313403d
Steve <steve2011avengers@gmail.com>
2020-03-10 19:47:31 +0530

    Added Authentication System

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nd2*f$r%!scynsd5z@y&__eto@5&$dv=pad5vsezg-0**^$m-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
